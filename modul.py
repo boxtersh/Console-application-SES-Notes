b1401ec0f857005b2bcf372b70157e359ab67514
Yuriy Terlyanskiy <boxtersh@yandex.ru>
2025-07-05 14:09:32 +0300

    feat (function table_note) Вывод заметок на экран

--- modul.py
+++ modul.py
@@ -2,3 +2,32 @@ import os, datetime, pickle, glob, locale, ui, validate, csv
 locale.setlocale(locale.LC_TIME, 'ru')
 
 
+# Вывод заметок на экран
+def table_note(collection: list) -> None:
+    """
+    :param collection: list список заметок
+    :return: None
+    """
+
+    for i in range(len(collection)):
+
+        for j in range(6):
+
+            if i == 0:
+                print(f'\033[33m{collection[i][j]: ^20}\033[0m', end='')
+
+            elif j == 2 and i != 0:
+
+                if collection[i][j] == 'срочная':
+                    print(f'\033[31m{collection[i][j]: ^20}\033[0m', end='')
+
+                elif collection[i][j] == 'важная':
+                    print(f'\033[35m{collection[i][j]: ^20}\033[0m', end='')
+
+                else:
+                    print(f'{collection[i][j]: ^20}', end='')
+
+            else:
+                print(f'{collection[i][j]: ^20}', end='')
+
+        print()
