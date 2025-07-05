68416c958122cd12205b231520bf19bf0b9cde77
Yuriy Terlyanskiy <boxtersh@yandex.ru>
2025-07-05 16:31:12 +0300

    feat function (choice_yes_no) Запрос да/нет от пользователя на выполнение действия

--- validate.py
+++ validate.py
@@ -9,3 +9,24 @@ def del_menu_item_symbol(line: str) -> str:
     line = line.strip(' ,.<>?/')
 
     return line
+
+# Запрос да/нет от пользователя на выполнение действия
+def choice_yes_no(result: str, message: str, error: str) -> bool:
+    """
+    :param result: Ввод от пользователя
+    :param message: действие программы требующее подтверждение
+    :param error: сообщение об ошибке в случае неверного ввода
+    :return: bool
+    """
+
+    result = del_menu_item_symbol(result)
+
+    while result not in ['Y', 'y', 'N', 'n']:
+        print(error)
+        result = input(message)
+        result = del_menu_item_symbol(result)
+
+    if result in ['Y', 'y']:
+        return True
+
+    return False
