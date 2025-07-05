bc9694e7f53f9836d31223be89069b39c1cd9d1b
Yuriy Terlyanskiy <boxtersh@yandex.ru>
2025-07-05 16:20:23 +0300

    feat function (del_menu_item_symbol) Удаление из ввода пользователя символов паразитов в начале и конце строки

--- validate.py
+++ validate.py
@@ -1,2 +1,11 @@
 import os
 
+# Удаление из ввода пользователя символов паразитов в начале и конце строки
+def del_menu_item_symbol(line: str) -> str:
+    """
+    :param line:
+    :return:
+    """
+    line = line.strip(' ,.<>?/')
+
+    return line
