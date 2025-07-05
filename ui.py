521e360ce289e54f137d6d5b97a66ae672a3ee7b
Yuriy Terlyanskiy <boxtersh@yandex.ru>
2025-07-05 15:09:21 +0300

    feat function (print_hi_bye) Вывод строки приветствия в начале и конце программы

--- ui.py
+++ ui.py
@@ -1,2 +1,22 @@
 import os, validate
 
+# Вывод строки приветствия в начале и конце программы
+def print_hi_bye(messag: str) -> None:
+    """
+    :param messag: Приветственное сообщение
+    :return: None
+    """
+    print(f'\n{messag}')
+
+
+
+
+
+
+
+
+
+
+
+
+
