c998210559cecb702ebf222baf32f8115dc189f4
Yuriy Terlyanskiy <boxtersh@yandex.ru>
2025-07-05 14:14:29 +0300

    feat (function add_notes_collection) Добавление заметок в сборник

--- modul.py
+++ modul.py
@@ -116,3 +116,94 @@ def sorting() -> None:
 
     sort_collection.insert(0, collection[0])
     table_note(sort_collection)
+
+# Добавление заметок в сборник
+def add_notes_collection() -> None:
+    """
+
+    :return: None
+    """
+    if not are_collections_yes_no():
+        ui.information_for_user(ui.dict_output_user['невозможное действие'])
+        return
+
+    file_collections()
+    name_files = requesting_file_name_from_directory(ui.dict_input_user['Добавить заметки'], ui.dict_input_error['нет файла'])
+
+    base_name = name_files + '.ses'
+    path_and_base_name = os.path.join(ui.path, base_name)
+
+    try:
+
+        with open(path_and_base_name, "rb") as file:
+            collection = pickle.load(file)
+
+    except:
+
+        ui.information_for_user(ui.dict_output_user['пустой'])
+        collection = create_new_note()
+        try:
+            with open(path_and_base_name, "wb") as file:
+                pickle.dump(collection, file)
+
+            ui.information_for_user(ui.dict_output_user['успешно заметка'])
+            return
+
+        except:
+            ui.information_for_user(ui.dict_output_user['не создан'])
+            return
+
+
+    if not isinstance(collection, list):
+
+        collection = create_new_note()
+
+    else:
+
+        if collection[0][6] != '':
+
+            password = input(ui.dict_input_user['пароль'])
+
+            while password != collection[0][6]:
+
+                ui.input_error(ui.dict_input_error['не верно пароль'])
+                password = input(ui.dict_input_user['пароль'])
+
+                if password == collection[0][6]:
+                    break
+
+                elif password == 'exit':
+                    return
+
+        name_note_in_list = ['']
+
+        for i in range (1, len(collection)):
+
+            name_note_in_list.append(collection[i][3])
+
+        collection = note(name_note_in_list, collection)
+
+        if collection[0][6] == '':
+
+            password_yes_no = input(ui.dict_input_user['пароль_y_n'])
+
+            if validate.choice_yes_no(password_yes_no, ui.dict_input_user['пароль_y_n'], ui.dict_input_error['y/n']):
+                password = input(ui.dict_input_user['пароль'])
+                collection[0][6] = password
+
+        else:
+            password_yes_no = input(ui.dict_input_user['пароль нет'])
+            if validate.choice_yes_no(password_yes_no, ui.dict_input_user['пароль нет'], ui.dict_input_error['y/n']):
+                collection[0][6] = ''
+
+
+        collection[0][7] = str(len(collection) - 1)
+
+    try:
+        with open(path_and_base_name, "wb") as file:
+            pickle.dump(collection, file)
+
+        ui.information_for_user(ui.dict_output_user['успешно заметка'])
+
+    except:
+        ui.information_for_user(ui.dict_output_user['не создан'])
